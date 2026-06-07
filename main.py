3b37ff584dc4de9424ae5bfb93e77cf60742b55b
kaloian3141 <kaloian70308@gmail.com>
2026-06-07 14:25:35 +0300

    създаване на kustomization за db

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from app.config import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
     await engine.dispose()
 
